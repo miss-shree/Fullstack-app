15702d199a77d7eff1af66dbf6a8a823fe2a1692
dhanwebllisto <dhanshreewebllisto01@gmail.com>
2020-05-31 23:37:04 +0530

    new changes

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var server= http.createServer(function(res,req){
+    fs.readFile('index.html', function(err,data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
